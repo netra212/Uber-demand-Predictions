eaf08b7750fd4eea402c541386a502db177ece50
netr212 <netra200021kcbdr@gmail.com>
2025-04-07 20:01:22 +0100

    testing model performance

---
+++ tests/test_model_registry.py
@@ -0,0 +1,25 @@
+import mlflow
+import dagshub
+import json
+
+import dagshub
+dagshub.init(repo_owner='netra200021kcbdr', repo_name='Uber-demand-Predictions', mlflow=True)
+
+# set the mlflow tracking uri
+mlflow.set_tracking_uri("https://dagshub.com/netra200021kcbdr/Uber-demand-Predictions.mlflow")
+
+def load_model_information(file_path):
+    with open(file_path) as f:
+        run_info = json.load(f)
+        
+    return run_info
+
+# set model name
+model_path = load_model_information("run_information.json")["model_uri"]
+
+# load the latest model from model registry
+model = mlflow.sklearn.load_model(model_path)
+
+def test_load_model_from_registry():
+    assert model is not None, "Failed to load model from registry"
+    
